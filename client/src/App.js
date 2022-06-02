534e282eada07709b5b1020d63913dd48bc6db40
colin brindle <hcolinbrindle@gmail.com>
2022-06-01 23:33:39 -0400

    Created login and sign up forms, routes, and logic. Issues linking to database

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,83 @@
-import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Login, HomePage, Fight, FightChoose, ChampionSelect, CreateChampion, Store } from './components/pages'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import "./App.css";
+
+import {
+  Login,
+  Signup,
+  HomePage,
+  Fight,
+  FightChoose,
+  ChampionSelect,
+  CreateChampion,
+  Store,
+} from "./components/pages";
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <h1>Link navbar element here</h1>
+
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/store" element={<Store />} />
+            <Route path="/championselect" element={<ChampionSelect />} />
+            <Route path="/fight" element={<Fight />} />
+            <Route path="/fightChoice" element={<FightChoose />} />
+            <Route path="/createchampion" element={<CreateChampion />} />
+          </Routes>
+        </>
+      </Router>
+    </ApolloProvider>
+
+    /*
     <div className="App">
       <Router>
         <h1>link navbar element here</h1>
 
         <Routes>
-          <Route
-            path='/'
-            element={<HomePage/>}
-            />
-            <Route
-              path='/login'
-              element={<Login/>}
-              />
-              <Route
-              path='/store'
-              element={<Store/>}
-              />
-              <Route
-              path='/championselect'
-              element={<ChampionSelect/>}
-              />
-              <Route
-              path='/fight'
-              element={<Fight/>}
-              />
-              <Route
-              path='/fightchoice'
-              element={<FightChoose/>}
-              />
-              <Route
-              path='/createchampion'
-              element={<CreateChampion/>}
-              />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/championselect" element={<ChampionSelect />} />
+          <Route path="/fight" element={<Fight />} />
+          <Route path="/fightchoice" element={<FightChoose />} />
+          <Route path="/createchampion" element={<CreateChampion />} />
         </Routes>
       </Router>
     </div>
+    */
   );
 }
 
--- client/src/components/pages/Store.js
+++ client/src/components/pages/Store.js
@@ -2,10 +2,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function Store () {
-    return (
-        <div>
-            <Link to='/championselect'>Select Champion</Link>
-        </div>
-    )
+export default function Store() {
+  return (
+    <div>
+      <Link to="/championselect">Select Champion</Link>
+    </div>
+  );
 }
