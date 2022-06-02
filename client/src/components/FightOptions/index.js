d05c52c3c10747baca293fd5072adf3a0c79681b
Brian Samuels <samuels.brian9@gmail.com>
2022-06-02 12:21:17 -0400

    pulling changes

--- client/src/components/FightOptions/index.js
+++ client/src/components/FightOptions/index.js
@@ -2,7 +2,7 @@ import '../../index.css'
 
 export const FightOptions = ({ onAttack, onDefend }) => (
     <div>
-        <div onClick={onAttack} className='playerCards'>Attack</div>
-        <div onClick={onDefend} className='playerCards'>Defend</div>
+        <div onClick={onAttack} className='fightOptions'>Attack</div>
+        <div onClick={onDefend} className='fightOptions'>Defend</div>
     </div>
 )
