bb785a58f8c5bcd3f30bf83e63581ab59f3a4581
colin brindle <hcolinbrindle@gmail.com>
2022-06-01 23:36:02 -0400

    Removed some previously commented code

--- client/src/App.js
+++ client/src/App.js
@@ -60,24 +60,6 @@ function App() {
         </>
       </Router>
     </ApolloProvider>
-
-    /*
-    <div className="App">
-      <Router>
-        <h1>link navbar element here</h1>
-
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/store" element={<Store />} />
-          <Route path="/championselect" element={<ChampionSelect />} />
-          <Route path="/fight" element={<Fight />} />
-          <Route path="/fightchoice" element={<FightChoose />} />
-          <Route path="/createchampion" element={<CreateChampion />} />
-        </Routes>
-      </Router>
-    </div>
-    */
   );
 }
 
