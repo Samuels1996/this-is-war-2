b8b8661a08589ddca2c526dd775c40ac1bd4984b
Seraphina <etanschool@gmail.com>
2022-06-03 10:37:49 -0400

    add item mutation

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,10 +6,6 @@ const expiration = "2h";
 module.exports = {
   authMiddleware: function ({ req }) {
     let token = req.body.token || req.query.token || req.headers.authorization;
-<<<<<<< HEAD
-    if (req.headers.authorization) {
-      token = token.split(" ").pop().trim();
-=======
 
     console.log(`Headers ${req.headers.authorization}`);
 
@@ -17,7 +13,6 @@ module.exports = {
       token = token.split(" ").pop().trim();
 
       console.log(token);
->>>>>>> 78643c82fbd6cdf26f940bb723f90dcbb5311ee9
     }
 
     if (!token) {
