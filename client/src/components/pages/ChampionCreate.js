386a64fa1998c97e6e2730bd100c9c833f07646a
colin brindle <hcolinbrindle@gmail.com>
2022-06-04 15:29:19 -0400

    Updated champion create styling and navbar linkage

--- client/src/components/pages/ChampionCreate.js
+++ client/src/components/pages/ChampionCreate.js
@@ -5,6 +5,9 @@ import { ADD_CHARACTER_TO_USER, CREATE_CHARACTER } from "../../utils/mutations";
 import { useMutation } from "@apollo/client";
 import AuthService from "../../utils/auth";
 import { useEffect } from "react";
+import archer from "../../images/megaman-thing.png";
+import warrior from "../../images/warrior.png";
+import wizard from "../../images/wizard.png";
 
 export default function CreateChampion() {
   const [character, setCharacter] = useState({
@@ -39,87 +42,90 @@ export default function CreateChampion() {
     // user(userId, { name: name, classType: charType });
     setCharType("Select type");
     document.getElementById("name").value = "";
+    window.location.pathname = "/fightchoice";
   };
+
   const characterImg = () => {
-    if (charType === "Wizard") {
-      return <img src="" alt="Wizard Avatar" />;
-    } else if (charType === "Archer") {
-      return <img src="" alt="Archer Avatar" />;
+    if (charType === "Champion Class: Wizard") {
+      return <img src={wizard} alt="Wizard Avatar" className="character-img" />;
+    } else if (charType === "Champion Class: Archer") {
+      return <img src={archer} alt="Archer Avatar" className="character-img" />;
     } else {
-      return <img src="" alt="Warrior Avatar" />;
+      return (
+        <img src={warrior} alt="Warrior Avatar" className="character-img" />
+      );
     }
   };
   return (
-    <div className="row">
-      <div>
-        <Link to="/championselect">Champion Select</Link>
-      </div>
-      <div className="char">
-        <h2>Character Creation</h2>
-        <form action="" className="">
-          <label htmlFor="">Name</label>
-          <input type="text" placeholder="Character Name" id="name" />
-          <div className="dropdown my-2">
-            <a
-              className="btn btn-secondary dropdown-toggle"
-              href="#"
-              role="button"
-              id="dropdownMenuLink"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-              onClick={() => changeshow(!show)}
-            >
-              {charType}
-            </a>
+    <div className="container character-creation site-font">
+      <div className="row">
+        <div className="creation-box">
+          <h1>Champion Creation</h1>
+          <form>
+            <label htmlFor="">Champion Name: </label>
+            <input type="text" placeholder="Character Name" id="name" />
+            <div className="dropdown my-2">
+              <a
+                className="btn btn-secondary dropdown-toggle"
+                href="#"
+                role="button"
+                id="dropdownMenuLink"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+                onClick={() => changeshow(!show)}
+              >
+                {charType}
+              </a>
 
-            <ul className={menuClass} aria-labelledby="dropdownMenuLink">
-              <li>
-                <button
-                  className="dropdown-item"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    changeType("Wizard");
-                  }}
-                >
-                  Wizard
-                </button>
-              </li>
-              <li>
-                <button
-                  className="dropdown-item"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    changeType("Archer");
-                  }}
-                >
-                  Archer
-                </button>
-              </li>
-              <li>
-                <button
-                  className="dropdown-item"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    changeType("Warrior");
-                  }}
-                >
-                  Warrior
-                </button>
-              </li>
-            </ul>
-          </div>
-          <button
-            className="btn btn-primary"
-            onClick={(e) => {
-              e.preventDefault();
-              handleSubmit();
-            }}
-          >
-            submit
-          </button>
-        </form>
+              <ul className={menuClass} aria-labelledby="dropdownMenuLink">
+                <li>
+                  <button
+                    className="dropdown-item"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      changeType("Champion Class: Wizard");
+                    }}
+                  >
+                    Wizard
+                  </button>
+                </li>
+                <li>
+                  <button
+                    className="dropdown-item"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      changeType("Champion Class: Archer");
+                    }}
+                  >
+                    Archer
+                  </button>
+                </li>
+                <li>
+                  <button
+                    className="dropdown-item"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      changeType("Champion Class: Warrior");
+                    }}
+                  >
+                    Warrior
+                  </button>
+                </li>
+              </ul>
+            </div>
+            <div>{characterImg()}</div>
+            <button
+              className="btn btn-danger"
+              onClick={(e) => {
+                e.preventDefault();
+                handleSubmit();
+              }}
+            >
+              Create Champion
+            </button>
+          </form>
+        </div>
       </div>
-      <div>{characterImg()}</div>
     </div>
   );
 }
